77a04fd396a1fc0b594c20c0543a57c70df4c25a
pson0001 <pson0001@hotmail.com>
2018-08-28 23:13:10 +1000

    add population in map

--- oz-app/src/components/pages/parts/map.js
+++ oz-app/src/components/pages/parts/map.js
@@ -4,6 +4,12 @@ import MapGL, {FlyToInterpolator, Marker, Popup, NavigationControl} from 'react-
 import CityPin from './city-pin';
 import ControlPanel from './control-panel';
 import CITIES from './data_10.json';
+import jsondata from './ethnicity.geojson';
+import {defaultMapStyle, dataLayer} from './map-style.js';
+import {updatePercentiles} from './utils';
+import {fromJS} from 'immutable';
+import {json as requestJson} from 'd3-request';
+
 
 const MAPBOX_TOKEN = 'pk.eyJ1IjoicHNvbjAwMDEiLCJhIjoiY2pmeGZwdDc2NGEyNDMybnZuMDU0NTh6ZiJ9.NIPbcggFfW6c0tVUp9gvdA';
 
@@ -23,6 +29,10 @@ class MapSection extends Component{
       campusPre:'',
       popupInfo: null,
       popStyle: '',
+      mapStyle: defaultMapStyle,
+      year: 2015,
+      data: null,
+      hoveredFeature: null,
     }
   }
 
@@ -30,6 +40,12 @@ class MapSection extends Component{
     componentDidMount() {
     window.addEventListener('resize', this._resize);
     this._resize();
+    requestJson(jsondata, (error, response) => {
+      if (error) throw error;
+      if (!error) {
+        this._loadData(response);
+      }
+    });
     }
 
     componentWillUnmount() {
@@ -51,6 +67,32 @@ class MapSection extends Component{
         }
     }}
 
+    _loadData = data => {
+
+    updatePercentiles(data, f => f.properties.population);
+
+    const mapStyle = defaultMapStyle
+      // Add geojson source to map
+      .setIn(['sources', 'incomeByState'], fromJS({type: 'geojson', data}))
+      // Add point layer to map
+      .set('layers', defaultMapStyle.get('layers').push(dataLayer));
+
+    this.setState({data, mapStyle});
+  };
+
+  _updateSettings = (name, value) => {
+    if (name === 'year') {
+      this.setState({year: value});
+
+      const {data, mapStyle} = this.state;
+      if (data) {
+        updatePercentiles(data, f => f.properties.population[value]);
+        const newMapStyle = mapStyle.setIn(['sources', 'incomeByState', 'data'], fromJS(data));
+        this.setState({mapStyle: newMapStyle});
+      }
+    }
+  };
+
     _onViewportChange = viewport => this.setState({
     viewport: {...this.state.viewport, ...viewport}
     });
@@ -64,15 +106,16 @@ class MapSection extends Component{
     this.setState({viewport});
   };
 
-    _renderCityMarker = (city, index) => {
-      return (
-        <Marker key={`marker-${index}`}
-          longitude={city.longitude}
-          latitude={city.latitude} >
-          <CityPin size={15}/>
-        </Marker>
-      );
-    };
+  _renderCityMarker = (city, index) => {
+return (
+  <Marker key={`marker-${index}`}
+    longitude={city.longitude}
+    latitude={city.latitude} >
+    <CityPin size={15}/>
+  </Marker>
+);
+};
+
 
     _renderPopup() {
       const {popupInfo} = this.state;
@@ -91,15 +134,18 @@ class MapSection extends Component{
     this._onViewportChange({
       latitude: latitude,
       longitude: longitude,
-      zoom: 12,
+      zoom: 13,
       transitionInterpolator: new FlyToInterpolator(),
       transitionDuration: 800
     });
     };
     _onStyleChange = popStyle => this.setState({popStyle});
 
+handlebutton() {
+  console.log('button');
+}
   render(){
-    const {viewport, settings} = this.state;
+    const {viewport, settings, mapStyle} = this.state;
 
     return(
     <div>
@@ -107,16 +153,16 @@ class MapSection extends Component{
         <MapGL
          {...viewport}
          {...settings}
-         mapStyle="mapbox://styles/pson0001/cjl9jf0iv3g542rmsrpmemlei"
+         mapStyle='mapbox://styles/pson0001/cjl9jf0iv3g542rmsrpmemlei'
          onViewportChange={this._onViewportChange}
          dragToRotate={false}
          mapboxApiAccessToken={MAPBOX_TOKEN}>
-
+         <button onClick={this.pop}>Add</button>
 {CITIES.map(this._renderCityMarker)}
          {this._renderPopup()}
          <ControlPanel
          containerComponent={this.props.containerComponent}
-         onClick={this._onStyleChange} />
+         onClick={this._onStyleChange}></ControlPanel>
      </MapGL>
    </div>
     </div>
